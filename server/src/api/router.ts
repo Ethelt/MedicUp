b9b97fafa2f17b7ef7587f198fec827adbfadb3c
Bartłomiej Hada <bartlomiej.hada@gmail.com>
2024-12-31 02:14:19 +0100

    Updated to express 5 and fixed error handling

--- server/src/api/router.ts
+++ server/src/api/router.ts
@@ -1,4 +1,4 @@
-import { Express, Request, Response } from "express";
+import { Express, Request, Response, NextFunction } from "express";
 import { Database } from "../database/database";
 import { AuthRouter } from "./auth/auth.router";
 import { ApiRoutes } from "@medicup/shared";
@@ -7,6 +7,7 @@ export class Router {
   static registerRoutes(app: Express) {
     this.registerDefaultRoutes(app);
     AuthRouter.registerRoutes(app);
+    this.registerErrorHandlers(app);
   }
 
   private static registerDefaultRoutes(app: Express) {
@@ -19,9 +20,23 @@ export class Router {
           message: "Server is running",
           dbTime: result.rows[0].now,
         });
-      } catch (error) {
-        res.status(500).json({ error: "Database connection failed" });
+      } catch (error: any) {
+        res.status(500).json({ error: error.message });
       }
+    });
+  }
+
+  private static registerErrorHandlers(app: Express) {
+    // not found handler
+    app.use((_: Request, res: Response, __: NextFunction) => {
+      res.status(404).json({ message: "Not found" });
+    });
+
+    // default error handler
+    app.use((err: any, _: Request, res: Response, __: NextFunction) => {
+      res
+        .status(err.status || 500)
+        .json({ message: err.message ?? "Error occurred" });
     });
   }
 }
