4dd6e531f4b46630dbbe996e5e2f456d8328e9c7
avren <szymonurbanski217@gmail.com>
2025-01-11 21:05:06 +0100

    Test patient entry

--- server/setup.sql
+++ server/setup.sql
@@ -6,6 +6,9 @@ values ('doctor', '$2b$10$OmeNTv3yYPE1V5R6fFc8DOyFXlBsaAzoOXjET35zWLiPSCcnS5T.W'
 insert into registrar (login, password) values ('rejestrator', '$2b$10$EquuajpRz/TIiNWzzqAKO.grCEhO6pqvdnXwmzBQp4FOdtyGeFHWu');
 insert into registrar (login, password) values ('registrar', '$2b$10$wD2WznS1XxBYEVfGfYcwOOFQDOVaelJ4ze27bjSiOXo2pres8ahJW');
 
+INSERT INTO patient ("firstName", "lastName", "email", "phone", "password", "pesel") 
+VALUES ('patient1', 'patient2', 'patient1.patient2@test.com', '123456789', '$2b$10$oKU6bYi.XT2cBKbF8v7xseGzVYTZF3ToTWgaDb21sVoXWzksFUjfy', '12345678901');
+
 insert into visit ("patientId", "doctorId", "startAt", "endAt") values (1, 1, '2025-01-01T12:00:00Z', '2025-01-01T15:00:00Z');
 insert into visit ("patientId", "doctorId", "startAt", "endAt", "cancelledAt") 
 values (1, 2, '2025-01-01T13:00:00Z', '2025-01-01T15:00:00Z', '2025-01-01T10:00:00Z');
