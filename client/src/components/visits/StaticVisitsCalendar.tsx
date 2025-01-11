446b164450c4992f1974259cd06ba6c974e7c101
avren <szymonurbanski217@gmail.com>
2025-01-12 00:18:11 +0100

    Old StaticVisitsCalendar restore

--- client/src/components/visits/StaticVisitsCalendar.tsx
+++ client/src/components/visits/StaticVisitsCalendar.tsx
@@ -1,4 +1,3 @@
-import dayGridPlugin from "@fullcalendar/daygrid";
 import interactionPlugin from "@fullcalendar/interaction";
 import FullCalendar from "@fullcalendar/react";
 import timeGridPlugin from "@fullcalendar/timegrid";
@@ -9,8 +8,6 @@ type StaticVisitsCalendarProps = {
   handleEventClick: (visit: Visit) => void;
   visits: Visit[];
 };
-
-const cancelledVisitColor = "grey";
 
 export default function StaticVisitsCalendar(props: StaticVisitsCalendarProps) {
   const events = useMemo(
@@ -26,59 +23,41 @@ export default function StaticVisitsCalendar(props: StaticVisitsCalendarProps) {
   );
 
   return (
-    <div style={{ width: "100%", height: "100%" }}>
-      <div style={{ marginTop: 0, paddingTop: 0, height: 'calc(100% - 50px)' }}>
-        <FullCalendar
-          height="100%"
-          plugins={[timeGridPlugin, interactionPlugin, dayGridPlugin]}
-          initialView="timeGridWeek"
-          views={{
-            timeGridDay: {
-              type: "timeGrid",
-              duration: { days: 1 },
-              buttonText: "Dzień",
-            },
-            timeGridWeek: {
-              type: "timeGrid",
-              duration: { weeks: 1 },
-              buttonText: "Tydzień",
-            },
-            dayGridMonth: {
-              type: "dayGrid",
-              duration: { months: 1 },
-              buttonText: "Miesiąc",
-            },
-          }}
-          headerToolbar={{
-            start: "prev,next today",
-            center: "title",
-            end: "timeGridDay,timeGridWeek,dayGridMonth",
-          }}
-          slotLabelFormat={{
-            hour: "2-digit",
-            minute: "2-digit",
-            hour12: false,
-          }}
-          dayHeaderFormat={{
-            weekday: "short",
-            day: "numeric",
-            month: "short",
-            omitCommas: true,
-          }}
-          locale="pl"
-          editable={false}
-          selectable={false}
-          events={events}
-          eventClick={(e) => {
-            const visit = props.visits.find(
-              (visit) => visit.id.toString() === e.event.id
-            );
-            if (visit && !visit.cancelledAt) {
-              props.handleEventClick(visit);
-            }
-          }}
-        />
-      </div>
-    </div>
+    <FullCalendar
+      height={"100%"}
+      plugins={[timeGridPlugin, interactionPlugin]}
+      duration={{ week: 1 }}
+      allDaySlot={false}
+      slotMinTime={"06:00:00"}
+      slotMaxTime={"20:00:00"}
+      firstDay={1}
+      initialView="timeGrid"
+      slotLabelFormat={{
+        hour: "2-digit",
+        minute: "2-digit",
+        hour12: false,
+      }}
+      dayHeaderFormat={{
+        weekday: "short",
+        day: "numeric",
+        month: "short",
+        omitCommas: true,
+      }}
+      selectConstraint={{ startTime: "06:00:00", endTime: "20:00:00" }}
+      locale="pl"
+      editable={false}
+      selectable={false}
+      eventClick={(e) => {
+        const visit = props.visits.find(
+          (visit) => visit.id.toString() === e.event.id
+        );
+        if (visit && !visit.cancelledAt) {
+          props.handleEventClick(visit);
+        }
+      }}
+      events={events}
+    />
   );
 }
+
+const cancelledVisitColor = "grey";
