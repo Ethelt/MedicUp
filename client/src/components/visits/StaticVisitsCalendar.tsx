9bfcc9d00b857f51e75020643ba69cd4f265053a
avren <szymonurbanski217@gmail.com>
2025-01-11 19:37:45 +0100

    Added logo, removed unnecessary words, working on removing free space above logo

--- client/src/components/visits/StaticVisitsCalendar.tsx
+++ client/src/components/visits/StaticVisitsCalendar.tsx
@@ -1,9 +1,11 @@
-import dayGridPlugin from "@fullcalendar/daygrid"; // Plugin for month view
+import dayGridPlugin from "@fullcalendar/daygrid";
 import interactionPlugin from "@fullcalendar/interaction";
 import FullCalendar from "@fullcalendar/react";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import { Visit } from "@medicup/shared";
 import { useMemo } from "react";
+
+import logo from "../../assets/mlogo.svg";
 
 type StaticVisitsCalendarProps = {
   handleEventClick: (visit: Visit) => void;
@@ -27,56 +29,71 @@ export default function StaticVisitsCalendar(props: StaticVisitsCalendarProps) {
 
   return (
     <div style={{ width: "100%", height: "100%" }}>
-      <FullCalendar
-        height="100%"
-        plugins={[timeGridPlugin, interactionPlugin, dayGridPlugin]} // Added dayGridPlugin for month view
-        initialView="timeGridWeek" // Default view
-        views={{
-          timeGridDay: {
-            type: "timeGrid",
-            duration: { days: 1 },
-            buttonText: "Dzień", // Day view button
-          },
-          timeGridWeek: {
-            type: "timeGrid",
-            duration: { weeks: 1 },
-            buttonText: "Tydzień", // Week view button
-          },
-          dayGridMonth: {
-            type: "dayGrid",
-            duration: { months: 1 },
-            buttonText: "Miesiąc", // Month view button
-          },
-        }}
-        headerToolbar={{
-          start: "prev,next today", // Navigation buttons
-          center: "title", // Centered title
-          end: "timeGridDay,timeGridWeek,dayGridMonth", // Buttons for view selection
-        }}
-        slotLabelFormat={{
-          hour: "2-digit",
-          minute: "2-digit",
-          hour12: false,
-        }}
-        dayHeaderFormat={{
-          weekday: "short",
-          day: "numeric",
-          month: "short",
-          omitCommas: true,
-        }}
-        locale="pl"
-        editable={false}
-        selectable={false}
-        events={events}
-        eventClick={(e) => {
-          const visit = props.visits.find(
-            (visit) => visit.id.toString() === e.event.id
-          );
-          if (visit && !visit.cancelledAt) {
-            props.handleEventClick(visit);
-          }
-        }}
-      />
+      <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center", padding: "10px 0" }}>
+        <img
+          src={logo}
+          alt="Logo"
+          style={{ width: "50px", height: "50px", borderRadius: "5px" }}
+        />
+        <div>
+          <button>Button 1</button>
+          <button>Button 2</button>
+          <button>Button 3</button>
+        </div>
+      </div>
+
+      <div style={{ marginTop: 0, paddingTop: 0, height: 'calc(100% - 50px)' }}>
+        <FullCalendar
+          height="100%"
+          plugins={[timeGridPlugin, interactionPlugin, dayGridPlugin]}
+          initialView="timeGridWeek"
+          views={{
+            timeGridDay: {
+              type: "timeGrid",
+              duration: { days: 1 },
+              buttonText: "Dzień",
+            },
+            timeGridWeek: {
+              type: "timeGrid",
+              duration: { weeks: 1 },
+              buttonText: "Tydzień",
+            },
+            dayGridMonth: {
+              type: "dayGrid",
+              duration: { months: 1 },
+              buttonText: "Miesiąc",
+            },
+          }}
+          headerToolbar={{
+            start: "prev,next today",
+            center: "title",
+            end: "timeGridDay,timeGridWeek,dayGridMonth",
+          }}
+          slotLabelFormat={{
+            hour: "2-digit",
+            minute: "2-digit",
+            hour12: false,
+          }}
+          dayHeaderFormat={{
+            weekday: "short",
+            day: "numeric",
+            month: "short",
+            omitCommas: true,
+          }}
+          locale="pl"
+          editable={false}
+          selectable={false}
+          events={events}
+          eventClick={(e) => {
+            const visit = props.visits.find(
+              (visit) => visit.id.toString() === e.event.id
+            );
+            if (visit && !visit.cancelledAt) {
+              props.handleEventClick(visit);
+            }
+          }}
+        />
+      </div>
     </div>
   );
 }
--- client/src/theme/theme.ts
+++ client/src/theme/theme.ts
@@ -1,8 +1,8 @@
-import { createTheme } from "@mui/material";
-import "../index.css";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+import { createTheme } from "@mui/material";
+import "../index.css";
 
 export const theme = createTheme({});
