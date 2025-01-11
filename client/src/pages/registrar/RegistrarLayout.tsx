aad9781eed7042991e38a75a3e65a43530f6341c
avren <szymonurbanski217@gmail.com>
2025-01-12 00:08:22 +0100

    Consistent registrar header with patient and doctor

--- client/src/pages/registrar/RegistrarLayout.tsx
+++ client/src/pages/registrar/RegistrarLayout.tsx
@@ -35,36 +35,37 @@ export default function RegistrarLayout() {
 
   return (
     <RegistrarContext.Provider value={registrar}>
-      <Stack height="100%">
+      <Stack height="100vh" direction="column">
         {/* Header */}
-        <header>
-          <Box
-            sx={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "space-between", // Dodane, aby przyciski były rozmieszczone na krańcach
-              padding: "1rem",
-              backgroundColor: "#ffffff",
-              border: "2px solid lightgrey",
-            }}
+        <Stack
+          direction="row"
+          justifyContent="space-between"
+          alignItems="center"
+          padding={2}
+          border="2px solid lightgrey"
+          borderRadius={1}
+        >
+          {/* Logo */}
+          <svg
+            width="70px"
+            height="70px"
+            viewBox="0 0 193 229"
+            xmlns="http://www.w3.org/2000/svg"
           >
-            {/* Logo */}
-            <Box sx={{ marginRight: "1rem", width: "50px", height: "auto" }}>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 193 229"
-                width="100%"
-                height="100%"
-              >
-                <path
-                  style={{ fill: "#3765ff", fillOpacity: 1 }}
-                  d="M 0.3001292,75.380593 H 37.953427 L 57.972755,0.667449 109.93747,194.60242 138.49254,88.03344 h 55.06891 v 7.71067 H 145.53428 L 109.69998,229.47956 57.942579,36.318323 45.251496,83.68209 H 0.3001292 Z"
-                />
-              </svg>
-            </Box>
-            {/* Title */}
-            <Typography variant="h4" component="div"></Typography>
-            {/* Logout Button */}
+            <g>
+              <path
+                style={{
+                  fill: "#3765ff",
+                  fillOpacity: 1,
+                  strokeWidth: 0.264583,
+                }}
+                d="M 0.3001292,75.380593 H 37.953427 L 57.972755,0.667449 109.93747,194.60242 138.49254,88.03344 h 55.06891 v 7.71067 H 145.53428 L 109.69998,229.47956 57.942579,36.318323 45.251496,83.68209 H 0.3001292 Z"
+              />
+            </g>
+          </svg>
+
+          {/* Buttons */}
+          <Stack direction="row" gap={2} alignItems="center">
             <Button
               variant="contained"
               sx={{ backgroundColor: "black", color: "white", '&:hover': { backgroundColor: "#333" } }}
@@ -72,12 +73,12 @@ export default function RegistrarLayout() {
             >
               Wyloguj
             </Button>
-          </Box>
-        </header>
-
-        <Stack flex="1" overflow="auto">
-          <Outlet />
+          </Stack>
         </Stack>
+
+        <Box flexGrow={1} overflow="auto" position="relative" paddingBottom="50px">
+          <Outlet />
+        </Box>
 
         <Footer />
       </Stack>
