614f5543459a857f7a2bab3b7530779f5604fdba
avren <szymonurbanski217@gmail.com>
2025-01-12 19:54:11 +0100

    Text and logo in login screens

--- client/src/pages/auth/AuthLayout.tsx
+++ client/src/pages/auth/AuthLayout.tsx
@@ -8,30 +8,57 @@ export default function AuthLayout() {
       justifyContent="center"
       width="100%"
       height="100%"
-      spacing={4}
+      spacing={2}
     >
       {/* Header Section */}
-      <Box
-        sx={{
-          display: "flex",
-          alignItems: "center",
-          position: "relative",
-          width: "100%",
-        }}
-      >
+      <Stack direction="row" alignItems="center" spacing={2}>
+        {/* Logo */}
+        <Box
+          component="div"
+          sx={{
+            width: 60,
+            height: 60,
+          }}
+        >
+          <svg
+            width="100%"
+            height="100%"
+            viewBox="0 0 193 229"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <g>
+              <path
+                style={{
+                  fill: "#3765ff",
+                  fillOpacity: 1,
+                  strokeWidth: 0.264583,
+                }}
+                d="M 0.3001292,75.380593 H 37.953427 L 57.972755,0.667449 109.93747,194.60242 138.49254,88.03344 h 55.06891 v 7.71067 H 145.53428 L 109.69998,229.47956 57.942579,36.318323 45.251496,83.68209 H 0.3001292 Z"
+              />
+            </g>
+          </svg>
+        </Box>
+
         {/* Text centered horizontally */}
         <Typography
           variant="h3"
           sx={{
             fontWeight: "bold",
             color: "#4a75ff",
-            textAlign: "center",
-            margin: "0 auto",
           }}
         >
           MedicUp
         </Typography>
-      </Box>
+
+        {/* Div to the right of the text, equal to the width of the logo */}
+        <Box
+          component="div"
+          sx={{
+            width: 60,
+            height: 1,
+          }}
+        />
+      </Stack>
 
       {/* Content Section */}
       <Outlet />
