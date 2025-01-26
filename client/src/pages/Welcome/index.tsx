1a3a047c5e9642487ac29889bf349d762037b93e
avren <szymonurbanski217@gmail.com>
2025-01-26 17:52:46 +0100

    Logo visible

--- client/src/pages/Welcome/index.tsx
+++ client/src/pages/Welcome/index.tsx
@@ -1,4 +1,5 @@
-import { Button, Stack, Typography } from "@mui/material";
+import { LoadingButton } from "@mui/lab";
+import { Box, Stack, Typography } from "@mui/material";
 import { useNavigate } from "react-router";
 import { AppRoutes } from "../../constants/AppRoutes";
 
@@ -10,23 +11,78 @@ export default function Welcome() {
             height="100vh"
             justifyContent="center"
             alignItems="center"
-            spacing={2}
+            spacing={4}
         >
-            <Typography variant="h4" fontWeight="bold">
-                Welcome to MedicUp
-            </Typography>
-            <Button
-                variant="contained"
-                onClick={() => navigate(AppRoutes.auth.login.patient)}
-            >
-                Login
-            </Button>
-            <Button
-                variant="contained"
-                onClick={() => navigate(AppRoutes.auth.register)}
-            >
-                Register
-            </Button>
+            {/* Header Section */}
+            <Stack direction="column" alignItems="center" spacing={2}>
+                <Typography variant="h5" fontWeight="medium">
+                    Dzień dobry, witamy w
+                </Typography>
+                <Stack direction="row" alignItems="center" spacing={2}>
+                    {/* Logo */}
+                    <Box
+                        component="div"
+                        sx={{
+                            width: 60,
+                            height: 60,
+                        }}
+                    >
+                        <svg
+                            width="100%"
+                            height="100%"
+                            viewBox="0 0 193 229"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <g>
+                                <path
+                                    style={{
+                                        fill: "#3765ff",
+                                        fillOpacity: 1,
+                                        strokeWidth: 0.264583,
+                                    }}
+                                    d="M 0.3001292,75.380593 H 37.953427 L 57.972755,0.667449 109.93747,194.60242 138.49254,88.03344 h 55.06891 v 7.71067 H 145.53428 L 109.69998,229.47956 57.942579,36.318323 45.251496,83.68209 H 0.3001292 Z"
+                                />
+                            </g>
+                        </svg>
+                    </Box>
+
+                    {/* Styled Text */}
+                    <Typography
+                        variant="h3"
+                        sx={{
+                            fontWeight: "bold",
+                            color: "#4a75ff",
+                        }}
+                    >
+                        MedicUp
+                    </Typography>
+
+                    {/* Empty Spacer */}
+                    <Box
+                        component="div"
+                        sx={{
+                            width: 60,
+                            height: 1,
+                        }}
+                    />
+                </Stack>
+            </Stack>
+
+            {/* Buttons Section */}
+            <Stack spacing={2} alignItems="center">
+                <LoadingButton
+                    variant="contained"
+                    onClick={() => navigate(AppRoutes.auth.login.patient)}
+                >
+                    Login
+                </LoadingButton>
+                <LoadingButton
+                    variant="contained"
+                    onClick={() => navigate(AppRoutes.auth.register)}
+                >
+                    Register
+                </LoadingButton>
+            </Stack>
         </Stack>
     );
 }
